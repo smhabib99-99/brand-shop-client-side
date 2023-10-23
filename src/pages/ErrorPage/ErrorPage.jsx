0c30897119fedba3fad096f0b72dfdee174b434d
smhabib99-99 <habibaust34@gmail.com>
2023-10-23 11:32:43 +0600

    ErrorPage implementation successful

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="">
+            <div className="flex h-screen text-5xl items-center justify-center">Oops!!!</div>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
         element:<Root></Root>,
+        errorElement:<ErrorPage></ErrorPage>,
         children: [
             {
                 path:'/',
