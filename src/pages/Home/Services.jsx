5301d5bc9a34e8a70cd7e73fb96a6c584d9aef8a
smhabib99-99 <habibaust34@gmail.com>
2023-10-23 11:17:34 +0600

    home page brand margin setup successful

--- src/pages/Home/Services.jsx
+++ src/pages/Home/Services.jsx
@@ -3,8 +3,8 @@ import ServiceCard from "./ServiceCard";
 
 const Services = ({services}) => {
     return (
-        <div>
-            <h2 className="text-center text-3xl bg-gray-900 py-5 mx-5 text-white">Brands We Sell</h2>
+        <div className="mx-5">
+            <h2 className="text-center text-3xl bg-gray-900 py-5 text-white">Brands We Sell</h2>
             <div className="grid md:grid-cols-3 block mx-auto gap-4 py-5 my-5">
                 {
                     services?.map(service=><ServiceCard key={service.id} service={service}></ServiceCard>)
