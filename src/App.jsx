08e5b4b1dd391e9adb0b2edcbf5707ae916c7930
smhabib99-99 <habibaust34@gmail.com>
2023-10-23 06:44:51 +0600

    brand shop client side project setup with required installation successful

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-3xl text-center'>
+        <h1>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTsmXM8S_Q90NYtnLtU81x3LeN6sBhl-E",
+  authDomain: "brand-shop-client-d5117.firebaseapp.com",
+  projectId: "brand-shop-client-d5117",
+  storageBucket: "brand-shop-client-d5117.appspot.com",
+  messagingSenderId: "660460134930",
+  appId: "1:660460134930:web:a2bfe56573ded7bedbeecb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
